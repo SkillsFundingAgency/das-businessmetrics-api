5ef0fe284adf92beab08091a4769773e3d9d0e7b
Balaji Jambulingam <balaji.j@hotmail.co.uk>
2026-08-21 16:28:17 +0100

    Unit test cases added

--- src/SFA.DAS.BusinessMetrics.Application.UnitTests/GetVacancyMetrics/GetVacancyMetricsQueryHandlerTest.cs
+++ src/SFA.DAS.BusinessMetrics.Application.UnitTests/GetVacancyMetrics/GetVacancyMetricsQueryHandlerTest.cs
@@ -1,4 +1,5 @@
 ﻿using SFA.DAS.BusinessMetrics.Application.GetVacancyMetrics.Queries;
+using SFA.DAS.BusinessMetrics.Domain.Constants;
 using SFA.DAS.BusinessMetrics.Domain.Interfaces.Services;
 using SFA.DAS.BusinessMetrics.Domain.Models;
 
@@ -19,5 +20,143 @@ namespace SFA.DAS.BusinessMetrics.Application.UnitTests.GetVacancyMetrics
 
             response.Result.VacancyMetrics.Count.Should().Be(result.Count);
         }
+
+        [Test, RecursiveMoqAutoData]
+        public async Task Handle_WhenMetricsReturned_GroupsByVacancyReference(
+            [Frozen] Mock<IVacancyMetricServices> metricServices,
+            GetVacancyMetricsQueryHandler sut,
+            GetVacancyMetricsQuery request)
+        {
+            var metrics = new List<VacancyMetrics>
+            {
+                new() { VacancyReference = "VAC001", Name = MetricConstants.Vacancy.Views, Count = 10 },
+                new() { VacancyReference = "VAC001", Name = MetricConstants.Vacancy.Started, Count = 5 },
+                new() { VacancyReference = "VAC002", Name = MetricConstants.Vacancy.Views, Count = 3 },
+            };
+
+            metricServices
+                .Setup(x => x.GetVacancyMetrics(request.StartDate, request.EndDate, CancellationToken.None))
+                .ReturnsAsync(metrics);
+
+            var response = await sut.Handle(request, CancellationToken.None);
+
+            response.Result.VacancyMetrics.Count.Should().Be(2);
+            response.Result.VacancyMetrics.Should().ContainSingle(x => x.VacancyReference == "VAC001");
+            response.Result.VacancyMetrics.Should().ContainSingle(x => x.VacancyReference == "VAC002");
+        }
+
+        [Test, RecursiveMoqAutoData]
+        public async Task Handle_WhenMetricsReturned_MapsAllMetricCountsCorrectly(
+            [Frozen] Mock<IVacancyMetricServices> metricServices,
+            GetVacancyMetricsQueryHandler sut,
+            GetVacancyMetricsQuery request)
+        {
+            const string vacancyRef = "VAC001";
+            var metrics = new List<VacancyMetrics>
+            {
+                new() { VacancyReference = vacancyRef, Name = MetricConstants.Vacancy.Views, Count = 10 },
+                new() { VacancyReference = vacancyRef, Name = MetricConstants.Vacancy.Started, Count = 5 },
+                new() { VacancyReference = vacancyRef, Name = MetricConstants.Vacancy.Submitted, Count = 3 },
+                new() { VacancyReference = vacancyRef, Name = MetricConstants.Vacancy.SearchResults, Count = 20 },
+                new() { VacancyReference = vacancyRef, Name = MetricConstants.Vacancy.Saved, Count = 7 },
+            };
+
+            metricServices
+                .Setup(x => x.GetVacancyMetrics(request.StartDate, request.EndDate, CancellationToken.None))
+                .ReturnsAsync(metrics);
+
+            var response = await sut.Handle(request, CancellationToken.None);
+
+            var vacancyMetric = response.Result.VacancyMetrics.Single();
+            vacancyMetric.ViewsCount.Should().Be(10);
+            vacancyMetric.ApplicationStartedCount.Should().Be(5);
+            vacancyMetric.ApplicationSubmittedCount.Should().Be(3);
+            vacancyMetric.SearchResultsCount.Should().Be(20);
+            vacancyMetric.SavedCount.Should().Be(7);
+        }
+
+        [Test, RecursiveMoqAutoData]
+        public async Task Handle_WhenMetricNameCasingDiffers_StillMapsCorrectly(
+            [Frozen] Mock<IVacancyMetricServices> metricServices,
+            GetVacancyMetricsQueryHandler sut,
+            GetVacancyMetricsQuery request)
+        {
+            var metrics = new List<VacancyMetrics>
+            {
+                new() { VacancyReference = "VAC001", Name = MetricConstants.Vacancy.Views.ToUpper(), Count = 8 },
+                new() { VacancyReference = "VAC001", Name = MetricConstants.Vacancy.Started.ToLower(), Count = 4 },
+            };
+
+            metricServices
+                .Setup(x => x.GetVacancyMetrics(request.StartDate, request.EndDate, CancellationToken.None))
+                .ReturnsAsync(metrics);
+
+            var response = await sut.Handle(request, CancellationToken.None);
+
+            var vacancyMetric = response.Result.VacancyMetrics.Single();
+            vacancyMetric.ViewsCount.Should().Be(8);
+            vacancyMetric.ApplicationStartedCount.Should().Be(4);
+        }
+
+        [Test, RecursiveMoqAutoData]
+        public async Task Handle_WhenMetricNotPresent_DefaultsCountToZero(
+            [Frozen] Mock<IVacancyMetricServices> metricServices,
+            GetVacancyMetricsQueryHandler sut,
+            GetVacancyMetricsQuery request)
+        {
+            var metrics = new List<VacancyMetrics>
+            {
+                new() { VacancyReference = "VAC001", Name = MetricConstants.Vacancy.Views, Count = 5 },
+            };
+
+            metricServices
+                .Setup(x => x.GetVacancyMetrics(request.StartDate, request.EndDate, CancellationToken.None))
+                .ReturnsAsync(metrics);
+
+            var response = await sut.Handle(request, CancellationToken.None);
+
+            var vacancyMetric = response.Result.VacancyMetrics.Single();
+            vacancyMetric.ApplicationStartedCount.Should().Be(0);
+            vacancyMetric.ApplicationSubmittedCount.Should().Be(0);
+            vacancyMetric.SearchResultsCount.Should().Be(0);
+            vacancyMetric.SavedCount.Should().Be(0);
+        }
+
+        [Test, RecursiveMoqAutoData]
+        public async Task Handle_WhenNoMetricsReturned_ReturnsEmptyList(
+            [Frozen] Mock<IVacancyMetricServices> metricServices,
+            GetVacancyMetricsQueryHandler sut,
+            GetVacancyMetricsQuery request)
+        {
+            metricServices
+                .Setup(x => x.GetVacancyMetrics(request.StartDate, request.EndDate, CancellationToken.None))
+                .ReturnsAsync([]);
+
+            var response = await sut.Handle(request, CancellationToken.None);
+
+            response.Result.VacancyMetrics.Should().BeEmpty();
+        }
+
+        [Test, RecursiveMoqAutoData]
+        public async Task Handle_WhenMultipleMetricsExistForSameName_ReturnsFirst(
+            [Frozen] Mock<IVacancyMetricServices> metricServices,
+            GetVacancyMetricsQueryHandler sut,
+            GetVacancyMetricsQuery request)
+        {
+            var metrics = new List<VacancyMetrics>
+            {
+                new() { VacancyReference = "VAC001", Name = MetricConstants.Vacancy.Views, Count = 10 },
+                new() { VacancyReference = "VAC001", Name = MetricConstants.Vacancy.Views, Count = 99 },
+            };
+
+            metricServices
+                .Setup(x => x.GetVacancyMetrics(request.StartDate, request.EndDate, CancellationToken.None))
+                .ReturnsAsync(metrics);
+
+            var response = await sut.Handle(request, CancellationToken.None);
+
+            response.Result.VacancyMetrics.Single().ViewsCount.Should().Be(10);
+        }
     }
 }
+
--- src/SFA.DAS.BusinessMetrics.Domain.UnitTests/ServicesTests/VacancyMetricServicesTests.cs
+++ src/SFA.DAS.BusinessMetrics.Domain.UnitTests/ServicesTests/VacancyMetricServicesTests.cs
@@ -11,6 +11,27 @@ namespace SFA.DAS.BusinessMetrics.Domain.UnitTests.ServicesTests
 {
     public class VacancyMetricServicesTests
     {
+        private static LogsTable BuildLogsTable(IEnumerable<VacancyMetrics> vacancyMetrics)
+        {
+            var columns = new LogsTableColumn[]
+            {
+                MonitorQueryModelFactory.LogsTableColumn("CustomDimension", LogsColumnType.String),
+                MonitorQueryModelFactory.LogsTableColumn("Name", LogsColumnType.String),
+                MonitorQueryModelFactory.LogsTableColumn("Count", LogsColumnType.Long)
+            };
+
+            var rows = vacancyMetrics
+                .Select(m => MonitorQueryModelFactory.LogsTableRow(columns, [m.VacancyReference, m.Name, m.Count]))
+                .ToList();
+
+            return MonitorQueryModelFactory.LogsTable("tester", columns.AsEnumerable(), rows.AsEnumerable());
+        }
+
+        private static VacancyMetricServices BuildSut(
+            Mock<ILogsQueryClient> mockLogsQueryClient,
+            Mock<IOptions<LogAnalyticsWorkSpace>> mockLogAnalyticsWorkspace)
+            => new(mockLogAnalyticsWorkspace.Object, mockLogsQueryClient.Object);
+
         [Test, MoqAutoData]
         public async Task GetVacancyMetrics_Returns_Count_When_Config_Found(
             DateTime startDate,
@@ -43,6 +64,132 @@ namespace SFA.DAS.BusinessMetrics.Domain.UnitTests.ServicesTests
 
             actual.Count.Should().Be(vacancyMetrics.Count);
             actual.Should().BeEquivalentTo(vacancyMetrics);
+        }
+
+        [Test, MoqAutoData]
+        public async Task GetVacancyMetrics_WhenRowsReturned_MapsAllFieldsCorrectly(
+            DateTime startDate,
+            DateTime endDate,
+            List<VacancyMetrics> vacancyMetrics,
+            LogAnalyticsWorkSpace logAnalyticsWorkSpace,
+            [Frozen] Mock<ILogsQueryClient> mockLogsQueryClient,
+            [Frozen] Mock<IOptions<LogAnalyticsWorkSpace>> mockLogAnalyticsWorkspace)
+        {
+            mockLogAnalyticsWorkspace.Setup(x => x.Value).Returns(logAnalyticsWorkSpace);
+            mockLogsQueryClient
+                .Setup(x => x.ProcessQuery(It.IsAny<ResourceIdentifier>(), It.IsAny<string>(), It.IsAny<QueryTimeRange>(), CancellationToken.None))
+                .ReturnsAsync(BuildLogsTable(vacancyMetrics));
+
+            var actual = await BuildSut(mockLogsQueryClient, mockLogAnalyticsWorkspace)
+                .GetVacancyMetrics(startDate, endDate, CancellationToken.None);
+
+            actual.Should().BeEquivalentTo(vacancyMetrics);
+        }
+
+        [Test, MoqAutoData]
+        public async Task GetVacancyMetrics_WhenRowsReturned_ReturnsCorrectCount(
+            DateTime startDate,
+            DateTime endDate,
+            List<VacancyMetrics> vacancyMetrics,
+            LogAnalyticsWorkSpace logAnalyticsWorkSpace,
+            [Frozen] Mock<ILogsQueryClient> mockLogsQueryClient,
+            [Frozen] Mock<IOptions<LogAnalyticsWorkSpace>> mockLogAnalyticsWorkspace)
+        {
+            mockLogAnalyticsWorkspace.Setup(x => x.Value).Returns(logAnalyticsWorkSpace);
+            mockLogsQueryClient
+                .Setup(x => x.ProcessQuery(It.IsAny<ResourceIdentifier>(), It.IsAny<string>(), It.IsAny<QueryTimeRange>(), CancellationToken.None))
+                .ReturnsAsync(BuildLogsTable(vacancyMetrics));
+
+            var actual = await BuildSut(mockLogsQueryClient, mockLogAnalyticsWorkspace)
+                .GetVacancyMetrics(startDate, endDate, CancellationToken.None);
+
+            actual.Count.Should().Be(vacancyMetrics.Count);
+        }
+
+        [Test, MoqAutoData]
+        public async Task GetVacancyMetrics_WhenTableIsNull_ReturnsEmptyList(
+            DateTime startDate,
+            DateTime endDate,
+            LogAnalyticsWorkSpace logAnalyticsWorkSpace,
+            [Frozen] Mock<ILogsQueryClient> mockLogsQueryClient,
+            [Frozen] Mock<IOptions<LogAnalyticsWorkSpace>> mockLogAnalyticsWorkspace)
+        {
+            mockLogAnalyticsWorkspace.Setup(x => x.Value).Returns(logAnalyticsWorkSpace);
+            mockLogsQueryClient
+                .Setup(x => x.ProcessQuery(It.IsAny<ResourceIdentifier>(), It.IsAny<string>(), It.IsAny<QueryTimeRange>(), CancellationToken.None))
+                .ReturnsAsync((LogsTable)null!);
+
+            var actual = await BuildSut(mockLogsQueryClient, mockLogAnalyticsWorkspace)
+                .GetVacancyMetrics(startDate, endDate, CancellationToken.None);
+
+            actual.Should().BeEmpty();
+        }
+
+        [Test, MoqAutoData]
+        public async Task GetVacancyMetrics_WhenTableHasNoRows_ReturnsEmptyList(
+            DateTime startDate,
+            DateTime endDate,
+            LogAnalyticsWorkSpace logAnalyticsWorkSpace,
+            [Frozen] Mock<ILogsQueryClient> mockLogsQueryClient,
+            [Frozen] Mock<IOptions<LogAnalyticsWorkSpace>> mockLogAnalyticsWorkspace)
+        {
+            var emptyTable = BuildLogsTable([]);
+
+            mockLogAnalyticsWorkspace.Setup(x => x.Value).Returns(logAnalyticsWorkSpace);
+            mockLogsQueryClient
+                .Setup(x => x.ProcessQuery(It.IsAny<ResourceIdentifier>(), It.IsAny<string>(), It.IsAny<QueryTimeRange>(), CancellationToken.None))
+                .ReturnsAsync(emptyTable);
+
+            var actual = await BuildSut(mockLogsQueryClient, mockLogAnalyticsWorkspace)
+                .GetVacancyMetrics(startDate, endDate, CancellationToken.None);
+
+            actual.Should().BeEmpty();
+        }
+
+        [Test, MoqAutoData]
+        public async Task GetVacancyMetrics_PassesCorrectDateRangeToQueryClient(
+            DateTime startDate,
+            DateTime endDate,
+            LogAnalyticsWorkSpace logAnalyticsWorkSpace,
+            [Frozen] Mock<ILogsQueryClient> mockLogsQueryClient,
+            [Frozen] Mock<IOptions<LogAnalyticsWorkSpace>> mockLogAnalyticsWorkspace)
+        {
+            mockLogAnalyticsWorkspace.Setup(x => x.Value).Returns(logAnalyticsWorkSpace);
+            mockLogsQueryClient
+                .Setup(x => x.ProcessQuery(It.IsAny<ResourceIdentifier>(), It.IsAny<string>(), It.IsAny<QueryTimeRange>(), CancellationToken.None))
+                .ReturnsAsync(BuildLogsTable([]));
+
+            await BuildSut(mockLogsQueryClient, mockLogAnalyticsWorkspace)
+                .GetVacancyMetrics(startDate, endDate, CancellationToken.None);
+
+            mockLogsQueryClient.Verify(x => x.ProcessQuery(
+                It.IsAny<ResourceIdentifier>(),
+                It.IsAny<string>(),
+                It.Is<QueryTimeRange>(r => r.Start == startDate && r.End == endDate),
+                CancellationToken.None), Times.Once);
+        }
+
+        [Test, MoqAutoData]
+        public async Task GetVacancyMetrics_PassesCorrectResourceIdentifierToQueryClient(
+            DateTime startDate,
+            DateTime endDate,
+            LogAnalyticsWorkSpace logAnalyticsWorkSpace,
+            [Frozen] Mock<ILogsQueryClient> mockLogsQueryClient,
+            [Frozen] Mock<IOptions<LogAnalyticsWorkSpace>> mockLogAnalyticsWorkspace)
+        {
+            mockLogAnalyticsWorkspace.Setup(x => x.Value).Returns(logAnalyticsWorkSpace);
+            mockLogsQueryClient
+                .Setup(x => x.ProcessQuery(It.IsAny<ResourceIdentifier>(), It.IsAny<string>(), It.IsAny<QueryTimeRange>(), CancellationToken.None))
+                .ReturnsAsync(BuildLogsTable([]));
+
+            await BuildSut(mockLogsQueryClient, mockLogAnalyticsWorkspace)
+                .GetVacancyMetrics(startDate, endDate, CancellationToken.None);
+
+            mockLogsQueryClient.Verify(x => x.ProcessQuery(
+                It.Is<ResourceIdentifier>(r => r.ToString() == logAnalyticsWorkSpace.Identifier),
+                It.IsAny<string>(),
+                It.IsAny<QueryTimeRange>(),
+                CancellationToken.None), Times.Once);
         }
     }
 }
